9b405d736c21630e6482b0828e4fd156e41345b0
M. Reza Lavaryan <mrl.8081@gmail.com>
2016-11-11 15:06:35 +0800

    Adding MenuFactory

---
+++ src/MenuFactory.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Lavary\Menus;
+
+use Lavary\Menus\Matcher\Matcher;
+use Lavary\Menus\Matcher\MatcherInterface;
+use Lavary\Menus\Configuration\Configuration;
+
+class MenuFactory
+{
+    /**
+     * Menu collection
+     *
+     * @var Lavary\Menus\Collection
+     */
+    protected $collection;
+
+    /**
+     * Menu manager
+     *
+     * @var Lavary\Menus\Manager
+     */
+    protected $manager;
+ 
+    /**
+     * Initializing the menu builder
+     *
+     */
+    public function __construct(Configuration $configuration = null)
+    {
+        $this->collection = new Collection();
+        
+        if (is_null($configuration)) {
+            $configuration = new Configuration();
+        }
+
+        // Matcher wit ha default pattern
+        $matcher = new Matcher();
+        $matcher->addPattern(new \Lavary\Menus\Matcher\Pattern\UriPattern($_SERVER['REQUEST_URI']));
+
+        $this->manager = new Manager($configuration, $matcher);
+    }
+
+    /**
+     * Create a new menu
+     *
+     * @param  string  $name
+     * @param  callable  $callback
+     *
+     * @return \Lavary\Menus\Item
+     */
+    public function make($name, \Closure $callback)
+    {
+        if (is_callable($callback)) {
+            $menu = new Item($name, [], $this->manager);
+           
+            // Registering the items
+            call_user_func($callback, $menu);
+            
+            // Storing each menu instance in the collection
+            $this->collection->put($name, $menu);
+            
+            return $menu;
+        }
+    }
+
+    /**
+     * Sets the matcher object for this menu
+     *
+     * @param  \Lavary\Menus\Matcher\Matcher $matcher
+     *
+     * @return void
+     */
+    public function setMatcher(MatcherInterface $matcher)
+    {
+        $this->manager->setMatcher($matcher);
+    }
+
+    /**
+     * Sets the configuration settings
+     *
+     * @param  \Lavary\Menus\Configuration\Configuration $configuration
+     *
+     * @return void
+     */
+    public function setConfiguration(Configuration $configuration)
+    {
+        $this->manager->setConfiguration($configuration);
+    }
+
+    /**
+     * Get a menu by its title
+     *
+     * @param  string $title
+     *
+     * @return void
+     */
+    public function get($title)
+    {
+        return isset($this->collection[$title]) ? $this->collection[$title] : null;
+    }
+}
